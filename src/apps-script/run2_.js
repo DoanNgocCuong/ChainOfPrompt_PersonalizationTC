20dcae161c86a533f99563713c7cd2a4298bdb91
Doan Ngoc Cuong <cuongmkmtpgoldfinch@gmail.com>
2025-03-19 16:24:42 +0700

    CURSOR BÁ ĐẠO viết step run2_.js
    viết 1 file run2_.js
    Input là ô D2, Prompt E2, Output F2
    
    run2_.js chạy 3 luồng song song
    Input là ô D2, Prompt E2, Output F2
    Input là ô D3, Prompt E3, Output F3
    Input là ô D4, Prompt E4, Output F4

---
+++ src/apps-script/run2_.js
@@ -0,0 +1,48 @@
+function runMultiple() {
+  // Lấy bảng tính hiện tại
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = spreadsheet.getActiveSheet();
+
+  // Đọc input và prompt từ các ô
+  var inputs = [
+    { input: sheet.getRange('D2').getValue(), prompt: sheet.getRange('E2').getValue(), outputCell: 'F2' },
+    { input: sheet.getRange('D3').getValue(), prompt: sheet.getRange('E3').getValue(), outputCell: 'F3' },
+    { input: sheet.getRange('D4').getValue(), prompt: sheet.getRange('E4').getValue(), outputCell: 'F4' }
+  ];
+
+  // Kiểm tra xem các input và prompt có rỗng không
+  for (var i = 0; i < inputs.length; i++) {
+    if (!inputs[i].input || !inputs[i].prompt) {
+      Logger.log('Input hoặc prompt ở hàng ' + (i + 2) + ' không được rỗng.');
+      return;
+    }
+  }
+
+  // Lấy API key từ config
+  var apiKey = CONFIG.API_KEY;
+
+  // Tạo một mảng các promises cho các yêu cầu
+  var promises = inputs.map(function(item) {
+    return new Promise(function(resolve, reject) {
+      try {
+        // Gửi yêu cầu tới OpenAI và nhận phản hồi
+        var openAIResponse = getOpenAIResponse(item.prompt, item.input, apiKey);
+        // Ghi phản hồi vào ô tương ứng
+        writeResponseToSheet('main', item.outputCell, openAIResponse);
+        resolve();
+      } catch (error) {
+        Logger.log('Lỗi khi xử lý input ở ô ' + item.outputCell + ': ' + error.message);
+        reject(error);
+      }
+    });
+  });
+
+  // Chạy tất cả các promises song song
+  Promise.all(promises)
+    .then(function() {
+      Logger.log('Tất cả các yêu cầu đã hoàn thành.');
+    })
+    .catch(function(error) {
+      Logger.log('Có lỗi xảy ra: ' + error.message);
+    });
+} 
